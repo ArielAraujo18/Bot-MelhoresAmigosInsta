9a7d98fa1dfab40e6ca7516cc13d412eb6cb1651
Ariel Araújo <arielaraujo120@gmail.com>
2025-02-07 19:41:29 -0300

    07/02
    Teste

---
+++ teste.py
@@ -0,0 +1,14 @@
+import pyautogui
+import time
+
+time.sleep(3)
+x,y = pyautogui.position()
+print(x,y)
+#pyautogui.scroll(-1180)
+
+#novo loop
+#ime.sleep(1)
+#pyautogui.scroll(-940)
+
+#time.sleep(2)
+#pyautogui.click(x=1577, y=250)
